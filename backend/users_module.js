4616d3ed189300af96e52c4563a79646d30f8c87
Rakesh <sharmarakesh7271@gmail.com>
2022-01-30 14:13:23 +0530

    a

--- backend/users_module.js
+++ backend/users_module.js
@@ -2,8 +2,7 @@
 const mongoose = require("mongoose");
 //mongodb://127.0.0.1:27017/dbname
 //const conn_str = "mongodb://localhost:27017/tcet";
-const conn_str = "mongodb+srv://mdanassk:root@cluster0.rbpmf.mongodb.net/student?retryWrites=true&w=majority";
-//const conn_str = "mongodb://mdanassk:root@cluster0-shard-00-00.dslyw.mongodb.net:27017,cluster0-shard-00-01.dslyw.mongodb.net:27017,cluster0-shard-00-02.dslyw.mongodb.net:27017/student?ssl=true&replicaSet=atlas-3xk2hf-shard-0&authSource=admin&retryWrites=true&w=majority";
+const conn_str ="mongodb+srv://root:Rakesh88@cluster0.hndo6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose.connect(conn_str)
 .then(() => console.log("Connected successfully..."))
